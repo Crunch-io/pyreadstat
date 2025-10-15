b10bb9d4438b20b78cc869c5acc0946c431f1602
Alex Holst <alexander.holst@crunch.io>
2025-10-15 10:12:11 -0500

    Version 1.3.1.1 -> 1.3.1.2

--- setup.py
+++ setup.py
@@ -150,7 +150,7 @@ short_description = "Reads and Writes SAS, SPSS and Stata files into/from pandas
 
 setup(
     name='pyreadstat',
-    version='1.3.1.1',
+    version='1.3.1.2',
     description=short_description,
     author="Otto Fajardo",
     author_email="pleasecontactviagithub@notvalid.com",
